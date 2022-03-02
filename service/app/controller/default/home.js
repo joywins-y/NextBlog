abdf9062ca8b02ce0386a5c399f218fed02206be
windy <1519105541@qq.com>
2022-03-02 14:11:44 +0800

    数据获取

--- service/app/controller/default/home.js
+++ service/app/controller/default/home.js
@@ -15,42 +15,34 @@ class HomeController extends Controller {
     // const sql = 'SELECT article.id as id, article.title as title, article.introduce as introduce, article.addTime as addTime, article.view_count as view_count, type.typeName as typeName  FROM article LEFT JOIN type ON article.type_id = type.id';
     const sql = 'SELECT article.id as id, article.title as title, article.introduce as introduce, FROM_UNIXTIME(article.addTime, "%Y-%m-%d %H:%i:%s") as addTime, article.view_count as view_count, type.typeName as typeName  FROM article LEFT JOIN type ON article.type_id = type.id';
     const result = await this.app.mysql.query(sql);
-    this.ctx.body = {
-      data: result,
-    };
+    this.ctx.body = { data: result };
   }
 
   /** 根据文章ID查询文章详情 */
-  // async getArticleById() {
-  //   // 先配置路由的动态传值，然后再接收值
-  //   const id = this.ctx.params.id;
-  //   const sql = 'SELECT article.id as id, article.title as title, article.introduce as introduce, article.article_content as article_content, FROM_UNIXTIME(article.addTime, "%Y-%m-%d %H:%i:%s") as addTime, ' +
-  //             'article.view_count as view_count, type.typeName as typeName, type.id as typeId FROM article LEFT JOIN type ON article.type_id = type.id WHERE article.id = ' + id;
-  //   const result = await this.app.mysql.query(sql);
-  //   this.ctx.body = {
-  //     data: result,
-  //   };
-  // }
   async getArticleById() {
     // 先配置路由的动态传值，然后再接收值
     const id = this.ctx.params.id;
-
-    const sql = 'SELECT article.id as id,' +
-    'article.title as title,' +
-    'article.introduce as introduce,' +
-    'article.article_content as article_content,' +
-    "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime," +
-    'article.view_count as view_count ,' +
-    'type.typeName as typeName ,' +
-    'type.id as typeId ' +
-    'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
-    'WHERE article.id=' + id;
-    console.log('-=============================');
+    const sql = 'SELECT article.id as id, article.title as title, article.introduce as introduce, article.article_content as article_content, FROM_UNIXTIME(article.addTime, "%Y-%m-%d %H:%i:%s") as addTime, ' +
+              'article.view_count as view_count, type.typeName as typeName, type.id as typeId FROM article LEFT JOIN type ON article.type_id = type.id WHERE article.id = ' + id;
     const result = await this.app.mysql.query(sql);
-    console.log('-=============================');
-
     this.ctx.body = { data: result };
   }
+
+  /** 得到类别名称和编号 */
+  async getTypeInfo() {
+    const result = await this.app.mysql.select('type');
+    this.ctx.body = { data: result };
+  }
+
+  /** 根据类别ID获取文章列表 */
+  async getListById() {
+    const id = this.ctx.params.id;
+    const sql = 'SELECT article.id as id, article.title as title, article.introduce as introduce, FROM_UNIXTIME(article.addTime, "%Y-%m-%d %H:%i:%s") as addTime, article.view_count as view_count, ' +
+    'type.typeName as typeName FROM article LEFT JOIN type ON article.type_id = type.id WHERE type_id = ' + id;
+    const result = await this.app.mysql.query(sql);
+    this.ctx.body = { data: result };
+  }
+
 }
 
 module.exports = HomeController;
