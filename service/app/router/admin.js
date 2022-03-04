3d9410810e131d5d4cc8754ceb2e79661ea150ca
windy <1519105541@qq.com>
2022-03-04 19:18:43 +0800

    service: 文章管理

--- service/app/router/admin.js
+++ service/app/router/admin.js
@@ -7,4 +7,9 @@ module.exports = app => {
   router.get('/admin/index', controller.admin.main.index);
   router.get('/admin/checkOpenId', controller.admin.main.checkLogin);
   router.get('/admin/getTypeInfo', adminauth, controller.admin.main.getTypeInfo);
+  router.get('/admin/addArticle', adminauth, controller.admin.main.addArticle);
+  router.get('/admin/updateArticle', adminauth, controller.admin.main.updateArticle);
+  router.get('/admin/getArticleList', adminauth, controller.admin.main.getArticleList);
+  router.get('/admin/delArticle/:id', adminauth, controller.admin.main.delArticle);
+  router.get('/admin/getArticleById/:id', adminauth, controller.admin.main.getArticleById);
 };
